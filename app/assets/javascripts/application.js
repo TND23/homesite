44673af4c4a1d812bb22deae885f341ce049cf13
Tommy <tommyn@isomedia.com>
2014-02-17 19:06:45 -0800

    include tag link tag problem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require_tree ./views
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap
+//= require_tree .
